e241e0f280413069c9431cded4d04edd74575ea8
Nicole Holder <nholder06@gmail.com>
2019-12-18 19:38:50 -0600

    finished studio3 without bonuses

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,18 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+addCrewMember(candidate: object){
+  if(this.crew.length< 3 && !this.crew.includes(candidate)){
+    this.crew.push(candidate);
+    this.inCrew=true;
+  }else{
+    this.inCrew=false;
+  }
+  if(this.inCrew){
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index,1);
+  }else if(!this.inCrew && this.crew.length<3){
+    this.crew.push(member);
+  }
+}
 }
